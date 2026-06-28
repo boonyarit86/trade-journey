f4fc7783ba1f04591712641bd8a7479181bc5076
boonyarit <devboonyarit86@gmail.com>
2026-06-28 13:00:14 +0700

    BE:  feat(asset): create /asset/type/:id route for getting one

--- backend/src/asset/asset.controller.ts
+++ backend/src/asset/asset.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AssetService } from './asset.service';
 
 @Controller('asset')
 export class AssetController {
     constructor(private readonly assetService: AssetService) {}
 
-    @Get("type")
+    @Get("/type")
     getAllAssetType() {
         return this.assetService.getAllAssetTypes();
+    }
+
+    @Get("/type/:id")
+    getProjectById(@Param('id') id: string) {
+        return this.assetService.getAssetTypeById(id);
     }
 }
--- backend/src/asset/asset.service.ts
+++ backend/src/asset/asset.service.ts
@@ -25,5 +25,28 @@ export class AssetService {
             total: result.rowCount,
             data: assetTypes,
         };
-    }
+    };
+
+    async getAssetTypeById(id: string) {
+        const pool = this.pgService.getPool();
+        const result = await pool.query(
+            'SELECT * from "common"."CM01_AssetType" WHERE "CM01_Id" = $1',
+            [id],
+        );
+        const rows: IAssetTypeRow[] = result.rows;
+        if (rows.length === 0) throw new Error("Not found any asset type");
+
+        const assetTypes: IAssetType[] = rows.map((r) => ({
+            id: r.CM01_Id,
+            name: r.CM01_Name,
+            isActive: r.CM01_IsActive,
+            createdBy: r.CM01_CreatedBy,
+            createdAt: r.CM01_CreatedAt,
+            modifiedBy: r.CM01_ModifiedBy,
+            modifiedAt: r.CM01_ModifiedAt,
+        }))
+        return {
+            data: assetTypes[0],
+        };
+    };
 }
